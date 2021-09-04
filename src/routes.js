d95a9259f4b8e56c359db82d1064d4bc4b9050e7
NetoVilela <favneto.a.v.n@gmail.com>
2021-09-03 23:08:00 -0300

    Insere / como rota de login

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import LogadoPage from './pages/Logado';
 const Routes = () =>{
     return(
         <BrowserRouter>
+            <Route component = {LoginPage} path="/" exact />
             <Route component = {LoginPage} path="/login" exact />
             <Route component = {RegisterPage} path="/register" exact />
             <Route component = {EsqueciASenhaPage} path="/esqueci_a_senha" exact />
